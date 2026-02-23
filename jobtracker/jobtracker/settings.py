877ab285cad496d0de26d0d18c0fb3864a8274a8
Vindhya Sunkara <sunkaravindhya@gmail.com>
2026-02-23 15:24:04 +0530

    Update settings.py

--- jobtracker/jobtracker/settings.py
+++ jobtracker/jobtracker/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
